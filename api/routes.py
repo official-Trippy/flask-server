a0e7914b42d6b6d0e16541ce9462813cf6acb02c
DevYoung00 <dkdud203@naver.com>
2024-08-27 00:18:11 +0900

    fix: 임시로 에러 주석 처리

--- api/routes.py
+++ api/routes.py
@@ -3,7 +3,7 @@ import json
 from .weather import get_weather_list, get_nearest_location, read_branch, check_country, get_weather_info
 from response_dto import ReasonDTO, ErrorReasonDTO
 from .country import get_country_data, get_full_address
-from .recommend import  extract_keyword
+# from .recommend import  extract_keyword
 from urllib.parse import unquote
 
 api_bp = Blueprint('api', __name__)
@@ -71,30 +71,30 @@ def location():
             'message': 'INTERNAL_SERVER_ERROR',
             'detail': error_msg
         }), 500
-
-@api_bp.route('/recommend/post', methods=['POST'])
-def recommend():
-    try:
-        # jpype.attachThreadToJVM()
-        data = request.get_json()
-        print("Received data:", data)  # 데이터를 디버깅하기 위한 로그
-
-        # 데이터 처리 로직 작성
-        extracted_keywords = extract_keyword(data)
-
-        # 결과 출력'
-        result = ','.join(extracted_keywords)
-        print(result)
-        return Response(result, content_type='text/plain; charset=utf-8')
-        # print(f"Extracted Keywords: {', '.join(extracted_keywords)}")  # 전체 키워드를 출력
-        #
-        # return jsonify({"message": "Recommendation processed"}), 200
-    except Exception as e:
-        # 예외 처리
-        error_msg = f"Unexpected error occurred: {str(e)}"
-        return jsonify({
-            'success': False,
-            'error_code': 'COMMON500',
-            'message': 'INTERNAL_SERVER_ERROR',
-            'detail': error_msg
-        }), 500
+#
+# @api_bp.route('/recommend/post', methods=['POST'])
+# def recommend():
+#     try:
+#         # jpype.attachThreadToJVM()
+#         data = request.get_json()
+#         print("Received data:", data)  # 데이터를 디버깅하기 위한 로그
+#
+#         # 데이터 처리 로직 작성
+#         extracted_keywords = extract_keyword(data)
+#
+#         # 결과 출력'
+#         result = ','.join(extracted_keywords)
+#         print(result)
+#         return Response(result, content_type='text/plain; charset=utf-8')
+#         # print(f"Extracted Keywords: {', '.join(extracted_keywords)}")  # 전체 키워드를 출력
+#         #
+#         # return jsonify({"message": "Recommendation processed"}), 200
+#     except Exception as e:
+#         # 예외 처리
+#         error_msg = f"Unexpected error occurred: {str(e)}"
+#         return jsonify({
+#             'success': False,
+#             'error_code': 'COMMON500',
+#             'message': 'INTERNAL_SERVER_ERROR',
+#             'detail': error_msg
+#         }), 500
