7ba8ae69354fad74ab4328fadb39c96fb337ce9a
dh1010a <dh1010a@naver.com>
2024-10-02 12:51:10 +0900

    fix: errorDto 수정

--- response_dto.py
+++ response_dto.py
@@ -6,7 +6,8 @@ class ReasonDTO:
         self.result = result
 
 class ErrorReasonDTO:
-    def __init__(self, isSuccess, code, message):
+    def __init__(self, isSuccess, code, message, result=None):
         self.isSuccess = isSuccess
         self.code = code
         self.message = message
+        self.result = result
